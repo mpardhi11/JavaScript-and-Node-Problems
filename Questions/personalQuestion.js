7da85a7a8b19a6951efdd3321a9e0308223a41b2
Mohit Pardhi <mpardhi11@gmail.com>
2023-05-10 00:38:37 +0530

    new question added

--- Questions/personalQuestion.js
+++ Questions/personalQuestion.js
@@ -59,8 +59,8 @@ console.log(guessTheOutput)
 const value1 = undefined
 const value2 = null
 
-console.log(value1 === value2) // ?
-console.log(value1 == value2) // ?
+console.log(value1 === value2)                          // ?
+console.log(value1 == value2)                           // ?
 */
 
 /* 
@@ -74,11 +74,9 @@ const obj2 = {
   mobile: 12345,
 };
 
-const obj3 = Object.assign({}, obj1, obj2); // guess the Output
-const obj4 = Object.assign(obj1, obj2); // guess the Output
+const obj3 = Object.assign({}, obj1, obj2);                 // guess the Output
+const obj4 = Object.assign(obj1, obj2);                     // guess the Output
 console.table([obj1, obj2, obj3, obj4]); 
-
-
 */
 
 /* 
@@ -86,8 +84,7 @@ const obj5 = Object.assign(<what is first argument>, <what is second argument>);
 */
 
 /*  
-
-// Guess the output why and
+// Guess the output why and what will be
 const obj1 = {
    name: "Mohit",
    age: 26,
@@ -98,11 +95,107 @@ const obj1 = {
    mobile: 12345,
  };
 
+
+ const obj3 = Object.assign({}, obj1, obj2);                // Guess the output why and what
  
- const obj3 = Object.assign({}, obj1, obj2); // Guess the output why and what
- 
- const obj4 = Object.assign(new Object(), obj1, obj2); // Guess the output why and what
-                    //  is it right or upper one is right
+ const obj4 = Object.assign(new Object(), obj1, obj2);      // Guess the output why and what
+                                                            //  is it right or upper one is right
  console.log(obj3);
  console.log(obj4); 
  */
+
+/* 
+const objArray = [
+  {
+    name: "Mohit",
+    age: 26,
+  },
+  {
+    address: "Pune",
+    mobile: 12345,
+  },
+  {
+    "first name": "Mohit",                                  // how will you access this property of object
+  },
+];
+
+console.log(); // write logic here
+*/
+
+/* 
+// check object property is present or not if else condition
+const obj1 = {
+   name: "Mohit",
+   age: 26,
+ };
+
+ if (!obj1.test) {                                          // it will work but not recommended
+
+   console.log(typeof obj1.test);                           // guess the type
+   console.log(obj1.test);                                  // guess the output
+   console.log("property is not present");
+ }
+
+ if (<whats other way>) {                                   // find other way its recommended 
+
+   console.log();                                           // typeof <data type of 2nd way>
+   console.log();                                           // return value <of 2nd way>
+   console.log("property is not present");
+ } 
+ */
+
+/*
+//  in 1 line destructure obj and rename name to firstName then log it
+ 
+ const obj = {
+    name :"Mohit"
+}
+
+const {} = obj                                          // do something to rename <name to firstName>. only in one line
+
+console.log(firstName); // Mohit 
+ */
+
+/* 
+// today is Tuesday 09-05-2023 question is 
+// from sun day to sat day make object like below it
+// when you wright code 
+// take start of week till end of week total 7 days 
+
+i need object in this from 
+const array = {
+    07-05-2023 :{
+        dayName:"sunday"
+    }
+    08-05-2023 :{
+        dayName:"monday"
+    }
+    09-05-2023 :{
+        dayName:"tuesday"
+    }
+    10-05-2023 :{
+        dayName:"wednesday"
+    }
+    11-05-2023 :{
+        dayName:"thursday"
+    }
+    12-05-2023 :{
+        dayName:"friday"
+    }
+    13-05-2023 :{
+        dayName:"saturday"
+    }
+} 
+*/
+
+/* 
+// JSON relates questions
+
+1) what is JSON
+2) full from of JSON
+3) JSON is ____ based data format
+4) method name which convert string to JSON
+4) what is used before JSON
+5) name data types supported by JSON tip:  total 6 datatype
+6) method name convert JSON to string
+*/
