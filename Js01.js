c1892d7fc39736796ee5d2a5a084967e29a1a3c7
Mohit Pardhi <mpardhi11@gmail.com>
2023-12-01 10:45:01 +0530

    01-12-2023

---
+++ Js01.js
@@ -0,0 +1,7 @@
+// See the below code, guess the output
+let num = "123";
+console.log("num", num); // num 123
+console.log("+num++", +num++); // +num++ 123
+console.log("num", num); // num 124
+console.log("num++", num++); // num++ 124
+console.log("num++", num); // num++ 125
---
+++ Js03.js
@@ -0,0 +1,4 @@
+let num2 = "5";
+let num3 = +num2++;
+console.log("num2", num2); // num2 6
+console.log("num3", num3); // num3 5
