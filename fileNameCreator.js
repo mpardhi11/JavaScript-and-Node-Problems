f639807037f64b9c5c4599ccca413879e15aab08
Mohit Pardhi <mpardhi11@gmail.com>
2024-03-14 21:23:33 +0530

    JS-Theory

--- fileNameCreator.js
+++ fileNameCreator.js
@@ -1,13 +1,16 @@
-import moment from "moment-timezone";
+import moment from 'moment-timezone';
 // Assuming you have moment.js library included in your project
 
 // Get the current timestamp in Unix format
 const unixTimestamp = moment().unix();
 
 // Format the date as per your requirement
-const formattedDate = moment().format("YYYY-MM-DD_HH_mm_ss_A");
+const formattedDate = moment().format('YYYY-MM-DD_HH_mm_ss_A');
 
 // Combine the components to create the desired string
-const resultString = `JavaScript-${formattedDate}-${unixTimestamp}`;
-
+let resultString = `JavaScript-${formattedDate}-${unixTimestamp}`;
+console.log(resultString);
+resultString = `Node-${formattedDate}-${unixTimestamp}`;
+console.log(resultString);
+resultString = `JS-Theory-${formattedDate}-${unixTimestamp}`;
 console.log(resultString);
