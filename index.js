8a0a46f4f932bc9f012995263b99f147468674a0
Mohit Pardhi <34490195+mpardhi11@users.noreplay.github.com>
2026-01-08 22:28:43 +0530

    aaa

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function memo() {
   let cache = {};
   return (value) => {
     if (value in cache) {
-      console.log("cache.value", cache[value]);
+      console.log('cache.value', cache[value]);
       return cache.value;
     } else {
       const result = value ** 2;
       cache[value] = result;
-      console.log("memo ~ result: 👆👆👆👆👆👆👆", result);
+      console.log('memo ~ result: 👆👆👆👆👆👆👆', result);
       return result;
     }
   };
